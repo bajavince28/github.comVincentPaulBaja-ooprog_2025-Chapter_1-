81dbf64613462150d73255291d620f2bec526f05
bajavince28 <bajavince28@gmail.com>
2025-09-16 08:40:14 +0800

    Add files via upload

---
+++ FavoriteSong.java
@@ -0,0 +1,8 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("Nandito ako, nag-aabang");
+        System.out.println("Nandiyan ka, nakangiti");
+        System.out.println("Ang puso ko'y tanging ikaw");
+        System.out.println("Nandito ako, nag-aabang pa");
+    }
+}
