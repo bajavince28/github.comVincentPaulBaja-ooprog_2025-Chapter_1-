b4b7f2253eaa8ec6fa25ab9beaf96ad40107dbe7
bajavince28 <bajavince28@gmail.com>
2025-09-18 08:44:14 +0800

    Add files via upload

---
+++ NameInput.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class NameInput {
+    public static void main(String[] args) {
+        while (true) {
+            String name = JOptionPane.showInputDialog("Enter your name:");
+            if (name == null) {
+                break;
+            }
+            int response = JOptionPane.showConfirmDialog(null, "Do you want to display your name?", "Confirmation", JOptionPane.YES_NO_OPTION);
+            if (response == JOptionPane.YES_OPTION) {
+                JOptionPane.showMessageDialog(null, "Your name is: " + name);
+                break;
+            }
+        }
+    }
+}
